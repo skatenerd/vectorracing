1bff913b37fde7dac498b280e4138bf97d3a6ea1
skatenerd <skatenerd@users.noreply.github.com>
2014-06-30 23:00:37 -0400

    Intersect segments

--- src/main.hs
+++ src/main.hs
@@ -1,19 +1,28 @@
 module Main where
+import Data.Bool
 
-data Point t = Point t t deriving (Show)
+data Point t = Point { p_x :: t, p_y :: t } deriving (Show)
 data Segment t = Segment (Point t) (Point t) deriving (Show)
-data Line t = Line t t deriving (Show)
+data Line t = Line { slope :: t,  intercept :: t } deriving (Show)
+data Vector t = Vector { v_x :: t, v_y :: t } deriving (Show)
+type Position = Point
 
-slope :: (Fractional t) => Segment t -> t
-slope (Segment (Point x1 y1) (Point x2 y2)) =
-  let rise = y2 - y1
-      run = x2 - x1
+
+in_domain x_value (Segment from to) = ((x_value < (p_x from)) && (x_value > (p_x to))) || ((x_value > (p_x from)) && (x_value < (p_x to)))
+
+in_domains :: (Ord t) => t -> [Segment t] -> Bool
+in_domains point segments = all (in_domain point) segments
+
+segment_slope :: (Fractional t) => Segment t -> t
+segment_slope (Segment from to) =
+  let rise = (p_y to) - (p_y from)
+      run = (p_x to) - (p_x from)
   in (rise / run)
 
 y_intercept slope (Point x y) = y - (slope * x)
 
 make_line :: (Fractional t) => (Segment t) -> (Line t)
-make_line segment = let theslope = slope segment
+make_line segment = let theslope = segment_slope segment
                         (Segment point _) = segment
                         intercept = y_intercept theslope point
                     in (Line theslope intercept)
@@ -22,23 +31,35 @@ value_at line x_value = let (Line slope intercept) = line
                         in (slope * x_value) + intercept
 
 intersection :: (Fractional t) => Line t -> Line t -> Point t
-intersection (Line firstSlope firstIntercept) (Line secondSlope secondIntercept) =
-  let lhsCoefficient = firstSlope - secondSlope
-      rhs = secondIntercept - firstIntercept
+intersection firstLine secondLine =
+  let lhsCoefficient = (slope firstLine) - (slope secondLine)
+      rhs = (intercept secondLine) - (intercept firstLine)
       x_intersection = rhs / lhsCoefficient
-      y_intersection = value_at (Line firstSlope firstIntercept) x_intersection
+      y_intersection = value_at firstLine x_intersection
   in Point x_intersection y_intersection
 
+segment_intersection :: (Fractional t, Ord t) => Segment t -> Segment t -> Maybe (Point t)
+segment_intersection first_segment second_segment = 
+  let first_line = make_line first_segment
+      second_line = make_line second_segment
+      the_intersection = intersection first_line second_line
+      legal = in_domains (p_x the_intersection) [first_segment, second_segment]
+  in if legal
+     then Just the_intersection
+     else Nothing
 
 main = let first = Point 2 0
            second = Point 3 2.5
            segment = Segment first second
            extendedSegment = make_line segment
-           flatLine = make_line $ Segment (Point 5 5) (Point 10 5)
+           flat_segment = Segment (Point 5 5) (Point 10 5)
+           flatLine = make_line $ flat_segment
  in do
    putStrLn $ show segment
-   putStrLn $ show $ slope segment
-   putStrLn $ show $ y_intercept (slope segment) first
+   putStrLn $ show $ segment_slope segment
+   putStrLn $ show $ y_intercept (segment_slope segment) first
    putStrLn $ show $ make_line segment
    putStrLn $ show flatLine
    putStrLn $ show $ intersection extendedSegment flatLine
+   putStrLn $ show $ segment_intersection segment flat_segment
+   putStrLn $ show $ segment_intersection (Segment (Point 5 0) (Point 10 10))  flat_segment
